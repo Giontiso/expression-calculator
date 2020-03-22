c8daf2621d04f60adabd202502a34242b14484a5
Egor Dubovik <Hamelion74@gmail.com>
2020-03-22 23:15:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,31 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    
+    let matrix;
+    matrix = expr.split("");
+    checkBrecketsPair(matrix);
+    checkDivisionByZero(matrix);
+    let number;
+    number = expr.replace(/\s/g, "");
+    let getResult;
+    getResult = new Function(`return ${number}`);
+    let result = getResult();
+    return result;
+  }
+  
+  function checkBrecketsPair(matrix) {
+    let Brackets = 0;
+    for (let value of matrix) {
+      if (value === "(") Brackets++;
+      if (value === ")") Brackets--;
+    }
+    if (!(Brackets === 0)) throw new Error("ExpressionError: Brackets must be paired.");
+  }
+  
+  function checkDivisionByZero(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[i] === "/" && matrix[i + 2] === "0") throw new Error("TypeError: Division by zero.");
+    } 
 }
 
 module.exports = {
